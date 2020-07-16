4e8b8fd04dc481f1324b2fa79579b0c6edd6e933
Timothy Lalli <trlalli92@gmail.com>
2020-07-16 13:16:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.all? {|call| "#{call.length < 3}"}
+  array.all? {|call| "#{call.length < 3}"}!
   array.any? {|call| "#{call.length > 4}"}
   
 end
