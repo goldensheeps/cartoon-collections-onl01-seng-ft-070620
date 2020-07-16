cc38b53c5574524205f4889c542fe8bd70a78e33
Timothy Lalli <trlalli92@gmail.com>
2020-07-16 13:09:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? {|call| "#{call.length > 4}"
-  true 
   array.all? {|call| "#{call.length <= 4}"
-  false 
+
 end
 
 def find_the_cheese# code an argument here
