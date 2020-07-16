e652a956043dc88b92f8b6766613ef31b6612a42
Timothy Lalli <trlalli92@gmail.com>
2020-07-16 12:56:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index {|dwarf, index| p "#{index + 1} #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|words| "#{words.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|call| "#{call.length > 4?"true":"false"}"} 
 end
 
 def find_the_cheese# code an argument here
