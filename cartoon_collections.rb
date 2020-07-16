0ccdf59fe695c7fa75bf7e4907dc37926d21999b
Timothy Lalli <trlalli92@gmail.com>
2020-07-16 13:11:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def long_planeteer_calls(array)
   array.any? {|call| "#{call.length > 4}"}
-  array.all? {|call| "#{call.length <= 4}"}
+  array.all? {|call| "#{call.length < 4}"}
 end
 
 def find_the_cheese# code an argument here
